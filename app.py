b260c7e6e8cc478ecfb3cd5fcb0de92b8d7a1290
Rohit Ranjan <rohitranjan0839@gmail.com>
2023-04-07 11:29:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
+import requests
+import time
+import xml.etree.ElementTree as ET
 from flask import Flask, render_template
-import feedparser
 
 app = Flask(__name__)
 
+# Set the RSS feed URL
+url = 'https://archives.nseindia.com/content/RSS/Corporate_action.xml'
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+
 @app.route('/')
 def index():
-   
-    # Set the RSS feed URL
-    rss_url = 'https://feeds.feedburner.com/nseindia/ca'
     
-    # Parse the RSS feed
-    feed = feedparser.parse(rss_url)
+    response = requests.get(url, headers=headers)
+    time.sleep(10)
+    xml_content = response.content
+    root = ET.fromstring(xml_content)
     
-    # Filtering to include only dividend corporate actions
-    keyword = 'DIVIDEND'
     filtered_items = []
-    for item in feed.entries:
-        if keyword in item.description.upper():
-            filtered_items.append(item)
+    items = root.findall('.//item')
+    keyword = 'DIVIDEND'
+    
+    # Filtering to only include dividend as corporate action
+    
+    for item in items:
+        description = item.find('description').text
+        if keyword in description:
+            title = item.find('title').text
+            filtered_items.append((title.replace('\n',''), description))
 
     # Render the template with the feed items
     return render_template('index.html', feed_items=filtered_items)
